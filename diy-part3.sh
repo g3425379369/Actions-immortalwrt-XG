055040a31aef25696dc789145c27220d237b6246
name <example@example.com>
2026-08-20 23:56:32 +0800

    1

--- diy-part3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# turboacc
-curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-
-cp -rf "$GITHUB_WORKSPACE/scripts/imm_XG-040G-MD_UBI/"* "$GITHUB_WORKSPACE/openwrt/"
-ls -R "$GITHUB_WORKSPACE/openwrt/files"
